7d481c9cab7aca049060cf1d0d6eabb8d931ce66
Eclésio Júnior <eclesiomelo.1@gmail.com>
2021-12-25 17:05:16 -0400

    chore: add `repl` to interact with the WIP lexer

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/EclesioMeloJunior/monkey-lang/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey Lang!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
